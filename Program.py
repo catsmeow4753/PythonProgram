cdafdc8e5f29e76a827f3959a63765e79ed90fbd
Jumpy Cat <77382504+catsmeow4753@users.noreply.github.com>
2022-11-24 13:18:01 +0100

    Update Program.py

--- Program.py
+++ Program.py
@@ -12,85 +12,25 @@ white = (255,255,255)
 nothing = (0,0,0)
 pink = (255,105, 180)
 
-def trinket_logo():
+def  creeper():
     G = green
     Y = yellow
     B = blue
-    O = nothing
-    logo = [
-    O, O, O, O, O, O, O, O,
-    O, Y, Y, Y, B, G, O, O,
-    Y, Y, Y, Y, Y, B, G, O,
-    Y, Y, Y, Y, Y, B, G, O,
-    Y, Y, Y, Y, Y, B, G, O,
-    Y, Y, Y, Y, Y, B, G, O,
-    O, Y, Y, Y, B, G, O, O,
-    O, O, O, O, O, O, O, O,
-    ]
-    return logo
-
-def raspi_logo():
-    G = green
-    R = red
-    O = nothing
-    logo = [
-    O, G, G, O, O, G, G, O, 
-    O, O, G, G, G, G, O, O,
-    O, O, R, R, R, R, O, O, 
-    O, R, R, R, R, R, R, O,
-    R, R, R, R, R, R, R, R,
-    R, R, R, R, R, R, R, R,
-    O, R, R, R, R, R, R, O,
-    O, O, R, R, R, R, O, O,
-    ]
-    return logo
-
-def plus():
     W = white
     O = nothing
     logo = [
-    O, O, O, O, O, O, O, O, 
-    O, O, O, W, W, O, O, O,
-    O, O, O, W, W, O, O, O, 
-    O, W, W, W, W, W, W, O,
-    O, W, W, W, W, W, W, O,
-    O, O, O, W, W, O, O, O,
-    O, O, O, W, W, O, O, O,
-    O, O, O, O, O, O, O, O,
+    G, G, G, G, G, G, G, G,
+    G, O, O, G, G, O, O, G,
+    G, O, O, G, G, O, O, G,
+    G, G, G, G, G, G, G, G,
+    G, G, G, O, O, G, G, G,
+    G, G, O, O, O, O, G, G,
+    G, G, O, G, G, O, G, G,
+    G, G, G, G, G, G, G, G,
     ]
     return logo
 
-def equals():
-    W = white
-    O = nothing
-    logo = [
-    O, O, O, O, O, O, O, O, 
-    O, W, W, W, W, W, W, O,
-    O, W, W, W, W, W, W, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, W, W, W, W, W, W, O,
-    O, W, W, W, W, W, W, O,
-    O, O, O, O, O, O, O, O,
-    ]
-    return logo
-
-def heart():
-    P = pink
-    O = nothing
-    logo = [
-    O, O, O, O, O, O, O, O,
-    O, P, P, O, P, P, O, O,
-    P, P, P, P, P, P, P, O,
-    P, P, P, P, P, P, P, O,
-    O, P, P, P, P, P, O, O,
-    O, O, P, P, P, O, O, O,
-    O, O, O, P, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    ]
-    return logo
-
-images = [trinket_logo, trinket_logo, plus, raspi_logo, raspi_logo, equals, heart, heart]
+images = [creeper]
 count = 0
 
 while True: 
