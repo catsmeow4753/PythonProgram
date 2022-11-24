844e1ae687180c64d5fa056c498d2b8520d483ea
Jumpy Cat <77382504+catsmeow4753@users.noreply.github.com>
2022-11-24 13:42:58 +0100

    Update Program.py

--- Program.py
+++ Program.py
@@ -8,15 +8,12 @@ green = (0, 255, 0)
 yellow = (255, 255, 0)
 blue = (0, 0, 255)
 red = (255, 0, 0)
-white = (255,255,255)
-nothing = (0,0,0)
-pink = (255,105, 180)
+white = (255, 255, 255)
+nothing = (0, 0, 0)
+pink = (255, 105, 180)
 
-def  creeper():
+def creeper():
     G = green
-    Y = yellow
-    B = blue
-    W = white
     O = nothing
     logo = [
     G, G, G, G, G, G, G, G,
@@ -29,11 +26,39 @@ def  creeper():
     G, G, G, G, G, G, G, G,
     ]
     return logo
+    
+def light():
+    W = white
+    logo = [
+    W, W, W, W, W, W, W, W,
+    W, W, W, W, W, W, W, W,
+    W, W, W, W, W, W, W, W,
+    W, W, W, W, W, W, W, W,
+    W, W, W, W, W, W, W, W,
+    W, W, W, W, W, W, W, W,
+    W, W, W, W, W, W, W, W,
+    W, W, W, W, W, W, W, W,
+    ]
+    return logo
+    
+def boom():
+    R = red
+    logo = [
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    ]
+    return logo
 
-images = [creeper]
+images = [creeper, creeper, creeper, light, creeper, light, creeper, light, boom, boom]
 count = 0
 
 while True: 
     s.set_pixels(images[count % len(images)]())
-    time.sleep(.75)
+    time.sleep(0.5)
     count += 1
